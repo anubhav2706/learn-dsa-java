69cfdc74cf221ae028f0080983b4db477ce0e50f
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-08 12:37:37 +0530

    initial commits

---
+++ src/dsa/basics/HelloWorld.java
@@ -0,0 +1,10 @@
+package dsa.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, DSA in Java!");
+	}
+
+}
