2c93dea3d5198074acc14b35424c79cc2c62cf12
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-10 21:28:29 +0530

    learn variables and data type

---
+++ src/dsa/basics/FirstClass.java
@@ -0,0 +1,22 @@
+package dsa.basics;
+
+import java.util.*;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter your name");
+		String head = sc.nextLine();
+//		System.out.println(head);
+		System.out.println("Hello "+head+ " Please enter two numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.print("The sume of two entered numbers: "+ a + " + " + b + " = " + sum);
+		
+		
+
+	}
+
+}
