bfa59577d82cba1f16bf6d0defb2246ca4966bce
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-11 16:40:43 +0530

    table of any input numbers till n times

--- src/dsa/basics/table.java
+++ src/dsa/basics/table.java
@@ -6,17 +6,19 @@ public class table {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number of which you wants to print table");
+		System.out.println("Enter the number for which you want to print the table:");
 		int n = sc.nextInt();
-		System.out.println("Enter the limit till you wants the table of that number");
+		System.out.println("Enter the multiplication times:");
 		int table = sc.nextInt();
 		if(n < 1 || table < 1) {
 			System.out.println("Enter the values greater than 1");
+			sc.close();
+			return;
 		}
 		for(int i = 1; i <= table; i++) {
-			System.out.println(n + " * "+ i + " = " + n*i);
+			System.out.println(n + " * " + i + " = " + (n * i));
 		}
-
+		sc.close();
 	}
 
 }
