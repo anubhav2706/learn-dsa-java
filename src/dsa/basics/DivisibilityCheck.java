96639c9646b90f3689632bb50e5baffda6cd9d63
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-18 21:39:33 +0530

    check number is divisble by 2, 3, 5

---
+++ src/dsa/basics/DivisibilityCheck.java
@@ -0,0 +1,38 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class DivisibilityCheck {
+
+    public static void checkDivisibility(int n) {
+        System.out.println("Checking divisibility for " + n + ":");
+
+        if (n % 2 == 0)
+            System.out.println(n + " is divisible by 2");
+        else
+            System.out.println(n + " is not divisible by 2");
+
+        if (n % 3 == 0)
+            System.out.println(n + " is divisible by 3");
+        else
+            System.out.println(n + " is not divisible by 3");
+
+        if (n % 5 == 0)
+            System.out.println(n + " is divisible by 5");
+        else
+            System.out.println(n + " is not divisible by 5");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        // Check divisibility
+        checkDivisibility(n);
+
+        sc.close();
+    }
+}
