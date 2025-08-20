e01690d80acd4630e389d9ae268aa85645f77757
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-20 21:08:52 +0530

    strong number

---
+++ src/dsa/basics/StrongNumberCheck.java
@@ -0,0 +1,43 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class StrongNumberCheck {
+
+    // Method to calculate factorial of a digit
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    // Method to check strong number
+    public static boolean isStrongNumber(int num) {
+        int sum = 0, temp = num;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isStrongNumber(num)) {
+            System.out.println(num + " is a Strong Number.");
+        } else {
+            System.out.println(num + " is NOT a Strong Number.");
+        }
+
+        sc.close();
+    }
+}
