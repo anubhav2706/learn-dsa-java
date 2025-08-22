21765db91e86aa5a5aa9f943dd5b04c86bba5aa8
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-22 23:14:39 +0530

    check duck number

---
+++ src/dsa/basics/DuckNumberCheck.java
@@ -0,0 +1,33 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class DuckNumberCheck {
+
+    public static boolean isDuckNumber(int num) {
+        String str = String.valueOf(num);
+
+        // Leading zero check
+        if (str.charAt(0) == '0') {
+            return false;
+        }
+
+        // Check if contains zero anywhere else
+        return str.contains("0");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isDuckNumber(num)) {
+            System.out.println(num + " is a Duck Number.");
+        } else {
+            System.out.println(num + " is NOT a Duck Number.");
+        }
+
+        sc.close();
+    }
+}
