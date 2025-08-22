c28399d4958b3aa110f69fa605efe7c144544bb6
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-22 23:07:28 +0530

    check harshad number

---
+++ src/dsa/basics/HarshadCheck.java
@@ -0,0 +1,32 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class HarshadCheck {
+
+    public static boolean isHarshad(int num) {
+        int sum = 0, temp = num;
+        
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        return num % sum == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isHarshad(num)) {
+            System.out.println(num + " is a Harshad Number.");
+        } else {
+            System.out.println(num + " is NOT a Harshad Number.");
+        }
+
+        sc.close();
+    }
+}
