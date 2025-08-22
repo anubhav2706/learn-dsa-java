c4210a2c2cd7bb0dd1fdf5a4eaf6b0ce0a8b44c7
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-22 23:12:38 +0530

    check spy number

---
+++ src/dsa/basics/SpyNumberCheck.java
@@ -0,0 +1,34 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class SpyNumberCheck {
+
+    public static boolean isSpyNumber(int num) {
+        int sum = 0, product = 1, temp = num;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit;
+            product *= digit;
+            temp /= 10;
+        }
+
+        return sum == product;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isSpyNumber(num)) {
+            System.out.println(num + " is a Spy Number.");
+        } else {
+            System.out.println(num + " is NOT a Spy Number.");
+        }
+
+        sc.close();
+    }
+}
