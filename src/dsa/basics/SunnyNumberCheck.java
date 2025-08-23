a29606652113e9ab6e1b2d34205be548a151a30d
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-23 17:47:26 +0530

    check number is sunny number

---
+++ src/dsa/basics/SunnyNumberCheck.java
@@ -0,0 +1,33 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class SunnyNumberCheck {
+
+    // Function to check if a number is perfect square
+    public static boolean isPerfectSquare(int n) {
+        int sqrt = (int)Math.sqrt(n);
+        return sqrt * sqrt == n;
+    }
+
+    // Function to check sunny number
+    public static boolean isSunny(int n) {
+        return isPerfectSquare(n + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isSunny(num)) {
+            System.out.println(num + " is a Sunny Number.");
+        } else {
+            System.out.println(num + " is NOT a Sunny Number.");
+        }
+
+        sc.close();
+    }
+}
+
