8b3838d838efaf6204540b1d0ad04526023f0868
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-24 19:51:57 +0530

    geometric progression of numbers

---
+++ src/dsa/basics/GeometricProgression.java
@@ -0,0 +1,33 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class GeometricProgression {
+
+    public static void printGP(int a, int r, int n) {
+        System.out.println("Geometric Progression:");
+        int term = a;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(term + " ");
+            term *= r;  // multiply by ratio
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first term (a): ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter common ratio (r): ");
+        int r = sc.nextInt();
+
+        System.out.print("Enter number of terms (n): ");
+        int n = sc.nextInt();
+
+        printGP(a, r, n);
+
+        sc.close();
+    }
+}
+
