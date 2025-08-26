1b05fce41b7f76e90a0826978e3b7a05bf5e099b
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-08-26 16:49:34 +0530

    decimal to octal

---
+++ src/dsa/basics/DecimalToOctal.java
@@ -0,0 +1,28 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class DecimalToOctal {
+
+    public static int decimalToOctal(int decimal) {
+        int octal = 0, place = 1;
+        while (decimal > 0) {
+            int remainder = decimal % 8;
+            octal += remainder * place;  // place = 1, 10, 100... (like octal digits)
+            place *= 10;
+            decimal /= 8;
+        }
+        return octal;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int decimal = sc.nextInt();
+
+        System.out.println("Octal representation = " + decimalToOctal(decimal));
+
+        sc.close();
+    }
+}
