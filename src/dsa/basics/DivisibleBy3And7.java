d2018a570683b5ebf23ee6e735b8701a343db1cd
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-09-20 12:55:46 +0530

    Print all numbers between 1 and n which are divisible by both 3 and 7

---
+++ src/dsa/basics/DivisibleBy3And7.java
@@ -0,0 +1,23 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class DivisibleBy3And7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Numbers between 1 and " + n + " divisible by both 3 and 7:");
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 21 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
+
