c5fa00d083bd8b836d65a5d8c5b6a119e91f231e
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2025-09-21 16:56:23 +0530

    twin prime numbers

---
+++ src/dsa/basics/TwinPrimes.java
@@ -0,0 +1,33 @@
+package dsa.basics;
+
+import java.util.Scanner;
+
+public class TwinPrimes {
+
+    // method to check prime
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        System.out.println("Twin primes up to " + n + ":");
+
+        for (int i = 2; i <= n - 2; i++) {
+            if (isPrime(i) && isPrime(i + 2)) {
+                System.out.println("(" + i + ", " + (i + 2) + ")");
+            }
+        }
+
+        sc.close();
+    }
+}
+
